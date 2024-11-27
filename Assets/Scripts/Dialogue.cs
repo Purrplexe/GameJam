8b6b35c5182031d0b2c883ea6b095a27acd49460
Purrplexe <piow375@gmail.com>
2024-11-27 12:17:34 +0100

    trying out how to do conversations
    added menu object that defines a conversation

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class Dialogue
+{
+    public int id;
+    public Dictionary<string, int> answerNavigation = new Dictionary<string, int>();
+    public string text;
+    public Sprite character1;
+    public Sprite character2;
+    public bool char1Talking;
+
+}
+[CreateAssetMenu(fileName = "new Dialogue", menuName = "Dialogue/DialogueFlow")]
+public class DialogueFlow : ScriptableObject
+{
+    public List<Dialogue> Dialogues;
+}
